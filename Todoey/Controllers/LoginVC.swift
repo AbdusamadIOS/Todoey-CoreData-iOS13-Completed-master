36dfa98370267bdd4ff95a2ddcc905508978d12d
Abdusamad <132121525+AbdusamadIOS@users.noreply.github.com>
2024-06-26 00:09:06 +0500

    keyboard

---
+++ Todoey/Controllers/LoginVC.swift
@@ -0,0 +1,30 @@
+//
+//  LoginVC.swift
+//  Todoey
+//
+//  Created by Abdusamad Mamasoliyev on 20/06/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Todoey/Controllers/ShowPhotoVC.swift
@@ -0,0 +1,30 @@
+//
+//  ShowPhotoVC.swift
+//  Todoey
+//
+//  Created by Abdusamad Mamasoliyev on 21/06/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ShowPhotoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
